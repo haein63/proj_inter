663be7c13a0b458bc4613ef73a4dfd463ff2be17
haein <haein@DESKTOP-TA6P4GH>
2018-06-14 15:39:38 +0900

    ..

--- src/main/java/com/homepage/config/BusinessLogicConfig.java
+++ src/main/java/com/homepage/config/BusinessLogicConfig.java
@@ -3,7 +3,6 @@ package com.homepage.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-
 @Configuration
 @ComponentScan({"com.homepage.dao", "com.homepage.service"})
 public class BusinessLogicConfig {
