b52bef591eb43f4599b4e2e082c9382df6140eea
ksyen <ksyen@LAPTOP-PO4P7JRQ>
2018-06-15 14:36:26 +0900

    dddddd

--- src/main/java/com/homepage/config/BusinessLogicConfig.java
+++ src/main/java/com/homepage/config/BusinessLogicConfig.java
@@ -2,7 +2,8 @@ package com.homepage.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.context.annotation.Import;
+@Import(InfraConfig.class)
 @Configuration
 @ComponentScan({"com.homepage.dao", "com.homepage.service"})
 public class BusinessLogicConfig {
